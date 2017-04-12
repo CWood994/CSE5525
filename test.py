73ce69ff133b1c918974ee91e4c60ef4e44bce46
Connor Wood <wood.994@osu.edu>
2017-04-11 21:12:40 -0400

    basic kmeans

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+from __future__ import division
 from sklearn.feature_extraction.text import CountVectorizer #pip install
 import os
+from sklearn.cluster import KMeans
+import numpy as np
+from sets import Set
+
 
 
 #unclassified tweets
@@ -34,6 +39,56 @@ def loadCategoryData():
     vocab = vec.fit_transform(tweets)
     return vocab,goldCategories
 
+def kmeans(vocab):
+    km = KMeans(15)
+    km.fit(vocab)
+    return km.labels_
+
+def accuracy(gold, pred):
+    print gold
+    print pred
+    correct_array = []
+    incorrect_array = []
+
+    categories = Set(pred)
+    for cat in categories:
+        indexes = [n for (n, e) in enumerate(pred) if e == cat]
+        guesses = [gold[index] for index in indexes]
+        guess = max(set(guesses), key=guesses.count)
+        correct = guesses.count(guess)
+        incorrect = len(guesses) - correct
+        correct_array.append(correct)
+        incorrect_array.append(incorrect)
+
+    print correct_array
+    print incorrect_array
+
+    #accuracy_array = [correct_array[i]/incorrect_array[i] for i in range(len(correct_array))]
+
+    return sum(correct_array) / sum(incorrect_array)
+
 if __name__ == "__main__":
    vocab,y_gold = loadCategoryData()
-   vocab = loadAllData()
+   #vocab = loadAllData()
+   y_pred = kmeans(vocab)
+   print accuracy(y_gold, y_pred)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
